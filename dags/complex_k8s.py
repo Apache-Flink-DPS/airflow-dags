7dec9c5c11f425214e0a73a4fe418dec47cb8d8a
Stefan Pedratscher <Stefan.Pedratscher@uibk.ac.at>
2025-09-11 09:35:29 +0200

    updated dag

--- dags/complex_k8s.py
+++ dags/complex_k8s.py
@@ -23,96 +23,90 @@ with DAG(
         tags=['k8s', 'complex'],
 ) as dag:
 
-    # Group 1: Initialization
-    with TaskGroup("init_group") as init_group:
-        init_hello = KubernetesPodOperator(
-            task_id='init_hello',
-            name='init-hello-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['print("Initializing pipeline...")'],
-            in_cluster=True,
-        )
+    init_hello = KubernetesPodOperator(
+        task_id='init_hello',
+        name='init-hello-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['print("Initializing pipeline...")'],
+        in_cluster=True,
+    )
 
-        init_date = KubernetesPodOperator(
-            task_id='init_date',
-            name='init-date-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['import datetime; print("Init time:", datetime.datetime.now())'],
-            in_cluster=True,
-        )
-
-        init_hello >> init_date
+    init_date = KubernetesPodOperator(
+        task_id='init_date',
+        name='init-date-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['import datetime; print("Init time:", datetime.datetime.now())'],
+        in_cluster=True,
+    )
 
     # Group 2: Processing
-    with TaskGroup("processing_group") as processing_group:
-        k8s_process_a = KubernetesPodOperator(
-            task_id='process_a',
-            name='process-a-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['import time; time.sleep(10); print("Processing task A")'],
-            env_vars={'STAGE': 'A'},
-            container_resources=k8s.V1ResourceRequirements(
-                requests={
-                    "cpu": 2,
-                    "memory": "200Mi",
-                },
-            ),
-            in_cluster=True,
-        )
+    k8s_process_a = KubernetesPodOperator(
+        task_id='process_a',
+        name='process-a-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['import time; time.sleep(10); print("Processing task A")'],
+        env_vars={'STAGE': 'A'},
+        container_resources=k8s.V1ResourceRequirements(
+            requests={
+                "cpu": 2,
+                "memory": "200Mi",
+            },
+        ),
+        in_cluster=True,
+    )
 
-        k8s_process_b = KubernetesPodOperator(
-            task_id='process_b',
-            name='process-b-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['print("Processing task B")'],
-            env_vars={'STAGE': 'B'},
-            container_resources=k8s.V1ResourceRequirements(
-                requests={
-                    "cpu": 1,
-                    "memory": "200Mi",
-                },
-            ),
-            in_cluster=True,
-        )
+    k8s_process_b = KubernetesPodOperator(
+        task_id='process_b',
+        name='process-b-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['print("Processing task B")'],
+        env_vars={'STAGE': 'B'},
+        container_resources=k8s.V1ResourceRequirements(
+            requests={
+                "cpu": 1,
+                "memory": "200Mi",
+            },
+        ),
+        in_cluster=True,
+    )
 
-        k8s_process_c = KubernetesPodOperator(
-            task_id='process_c',
-            name='process-c-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['import time; time.sleep(10); print("Finished C!!")'],
-            env_vars={'STAGE': 'C'},
-            container_resources=k8s.V1ResourceRequirements(
-                requests={
-                    "cpu": 10,
-                    "memory": "200Mi",
-                },
-            ),
-            in_cluster=True,
-        )
+    k8s_process_c = KubernetesPodOperator(
+        task_id='process_c',
+        name='process-c-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['import time; time.sleep(10); print("Finished C!!")'],
+        env_vars={'STAGE': 'C'},
+        container_resources=k8s.V1ResourceRequirements(
+            requests={
+                "cpu": 8,
+                "memory": "200Mi",
+            },
+        ),
+        in_cluster=True,
+    )
 
-        [k8s_process_a, k8s_process_b] >> k8s_process_c
+
 
     # Group 3: Finalization
-    with TaskGroup("final_group") as final_group:
-        k8s_finalize = KubernetesPodOperator(
-            task_id='finalize',
-            name='finalize-pod',
-            namespace='stefan-dev',
-            image='python:3.9-slim',
-            cmds=['python', '-c'],
-            arguments=['print("Pipeline completed successfully!")'],
-            in_cluster=True,
-        )
+    k8s_finalize = KubernetesPodOperator(
+        task_id='finalize',
+        name='finalize-pod',
+        namespace='stefan-dev',
+        image='python:3.9-slim',
+        cmds=['python', '-c'],
+        arguments=['print("Pipeline completed successfully!")'],
+        in_cluster=True,
+    )
 
     # DAG flow
-    init_group >> processing_group >> final_group
+    init_hello >> init_date >> [k8s_process_a, k8s_process_b] >> k8s_process_c >> k8s_finalize
